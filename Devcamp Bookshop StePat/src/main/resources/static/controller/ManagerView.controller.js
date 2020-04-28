476b1d1549535294c4e9639229826bde5fb2c6ab
I518273 <I518273@WDFN34686418A.emea.global.corp.sap>
2020-04-28 17:01:26 +0200

    Error while Create

--- Devcamp Bookshop StePat/src/main/resources/static/controller/ManagerView.controller.js
+++ Devcamp Bookshop StePat/src/main/resources/static/controller/ManagerView.controller.js
@@ -2,8 +2,9 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast",
 	"sap/ui/model/json/JSONModel",
-	"sap/ui/core/routing/History"
-], function (Controller, MessageToast, JSONModel, History) {
+	"sap/ui/core/routing/History",
+	"sap/ui/core/Fragment"
+], function (Controller, MessageToast, JSONModel, History, Fragment) {
 	"use strict";
 
 	return Controller.extend("BWATC.BookstoreWebAppTC.controller.ManagerView", {
@@ -21,7 +22,8 @@ sap.ui.define([
 			oRouter.navTo("detailManager",{
 				"isbn":oListItemData.isbn});
 //			MessageToast.show("Book: "+ oListItemData.isbn + " has been pressed")
-			},
+		},
+
 		onNavBack: function() {
 			var oHistory = History.getInstance();
 			var sPreviousHash = oHistory.getPreviousHash();
@@ -29,8 +31,36 @@ sap.ui.define([
 			if (sPreviousHash !== undefined) {
 				window.history.go(-1);
 			}
-		}
+		},
 	
+		_getDialog: function () {
+            // create dialog lazily
+            if (!this._oDialog) {
+                // create dialog via fragment factory
+                this._oDialog = sap.ui.xmlfragment("addBooksDialog", "BWATC.BookstoreWebAppTC.view.Create", this);
+                // connect dialog to view (models, lifecycle)
+                this.getView().addDependent(this._oDialog);
+            }
+            return this._oDialog;
+        },
+        
+        onCreate: function (oEvent) {
+            this._getDialog().open();
+        },
+       
+        onCloseDialog: function () {
+            this._getDialog().close();
+        },
+       
+        onSaveDialog: function (oEvent) {
+			// var testST = sap.ui.getCore().byId("newTitle").getValue();
+			var testST2 = this._getDialog("newTitle");
+            // var oSource = oEvent.getSource(),
+			// oListItemData=
+			// 	oSource.getObject();
+				
+			console.log(testST2);
+        }
 });
 	
 });
